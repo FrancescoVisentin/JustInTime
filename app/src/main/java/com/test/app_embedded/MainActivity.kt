d6b06ee01e41721d8e21f12b47077c8ea29fe446
FrancescoVisentin <74708171+FrancescoVisentin@users.noreply.github.com>
2022-04-29 11:17:14 +0200

    Initial commit

---
+++ app/src/main/java/com/test/app_embedded/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.test.app_embedded
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
