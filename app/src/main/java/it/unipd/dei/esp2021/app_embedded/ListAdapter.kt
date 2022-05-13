321db8f2b8eb0681b148e0edef2e719137cba045
FrancescoVisentin <74708171+FrancescoVisentin@users.noreply.github.com>
2022-05-13 10:22:12 +0200

    Inizio list

---
+++ app/src/main/java/it/unipd/dei/esp2021/app_embedded/ListAdapter.kt
@@ -0,0 +1,25 @@
+package it.unipd.dei.esp2021.app_embedded
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ListAdapter() : RecyclerView.Adapter<ListAdapter.ItemViewHolder>(){
+
+    class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+}
