8daf092630ffa8fec413ad3cbd11d5e9e01edeca
FrancescoVisentin <74708171+FrancescoVisentin@users.noreply.github.com>
2022-05-13 09:57:44 +0200

    Colori dinamici

--- app/src/main/java/it/unipd/dei/esp2021/app_embedded/AppEmbeddedApplication.kt
+++ app/src/main/java/it/unipd/dei/esp2021/app_embedded/AppEmbeddedApplication.kt
@@ -2,10 +2,15 @@ package it.unipd.dei.esp2021.app_embedded
 
 import android.app.Application
 import com.google.android.material.color.DynamicColors
+import com.google.android.material.color.DynamicColors.isDynamicColorAvailable
+import com.test.app_embedded.R
 
 class AppEmbeddedApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        //DynamicColors.applyToActivitiesIfAvailable(this)
+
+        if (isDynamicColorAvailable()) {
+            DynamicColors.applyToActivitiesIfAvailable(this)
+        }
     }
 }
