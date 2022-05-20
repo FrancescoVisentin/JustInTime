cf855890c0f6d10017315c764fbd8b17e0ad6090
Christian <christianmarch01@gmail.com>
2022-05-20 16:57:15 +0200

    Aggiunta cards (ricerca viaggio)

--- app/src/main/java/it/unipd/dei/esp2021/app_embedded/RicercaViaggioResultFragment.kt
+++ app/src/main/java/it/unipd/dei/esp2021/app_embedded/RicercaViaggioResultFragment.kt
@@ -6,7 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.navigation.findNavController
+import androidx.recyclerview.widget.RecyclerView
 import com.test.app_embedded.R
 
 class RicercaViaggioResultFragment : Fragment() {
@@ -15,6 +19,17 @@ class RicercaViaggioResultFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
 
         val view = inflater.inflate(R.layout.fragment_ricerca_viaggio_result, container, false)
+
+        val cardLayout = view.findViewById<LinearLayout>(R.id.linear)
+        for(i in 0..6)
+        {
+            val card = inflater.inflate(R.layout.card_layout, container, false) as CardView
+            card.setOnClickListener{
+                card.findViewById<TextView>(R.id.train_number).text = "Ciao mondo"
+            }
+            cardLayout.addView(card)
+        }
         return view
     }
+
 }
