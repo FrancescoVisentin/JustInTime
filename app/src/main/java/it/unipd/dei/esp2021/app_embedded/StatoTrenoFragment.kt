82140e1b8dea507658f9552e9023ac848eb85cfc
FrancescoVisentin <74708171+FrancescoVisentin@users.noreply.github.com>
2022-05-25 23:20:52 +0200

    stato treno

--- app/src/main/java/it/unipd/dei/esp2021/app_embedded/StatoTrenoFragment.kt
+++ app/src/main/java/it/unipd/dei/esp2021/app_embedded/StatoTrenoFragment.kt
@@ -5,15 +5,13 @@ import android.animation.AnimatorListenerAdapter
 import android.app.Activity
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.LinearLayout
-import android.widget.SeekBar
-import android.widget.TextView
+import android.widget.*
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
@@ -21,11 +19,14 @@ import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.transition.MaterialFadeThrough
 import com.test.app_embedded.R
+import it.unipd.dei.esp2021.app_embedded.helpers.HTTParser
+import it.unipd.dei.esp2021.app_embedded.helpers.TrainViewModel
 import java.text.SimpleDateFormat
 import java.util.*
 
 class StatoTrenoFragment : Fragment() {
-    private val model : HttpViewModel by activityViewModels()
+    private val model : TrainViewModel by activityViewModels()
+    private lateinit var resObserver : Observer<HTTParser.TrainInfo>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,6 +39,20 @@ class StatoTrenoFragment : Fragment() {
 
         val view = inflater.inflate(R.layout.fragment_stato_treno, container, false)
 
+        resObserver = Observer<HTTParser.TrainInfo> { info ->
+            if (info.trainID.compareTo("null") == 0) {
+                val trainView = view.findViewById<LinearLayout>(R.id.train_description)
+                trainView.visibility = View.INVISIBLE
+
+                val contextView = view.findViewById<View>(R.id.coordinator_layout)
+                Snackbar.make(contextView, "ID non valido", Snackbar.LENGTH_SHORT)
+                    .setAction("Chiudi") {}
+                    .show()
+                return@Observer
+            }
+            updateTrainInfo(info)
+        }
+
         val textView = view.findViewById<TextInputEditText>(R.id.text_train_number)
         textView.setOnEditorActionListener { v, actionId, _ ->
             if (actionId == EditorInfo.IME_ACTION_SEARCH) {
@@ -47,7 +62,7 @@ class StatoTrenoFragment : Fragment() {
                 imm?.hideSoftInputFromWindow(v.windowToken, 0)
 
                 if (textView.text.toString().isNotEmpty()) {
-                    searchTrain(textView.text.toString())
+                    model.searchTrain(textView.text.toString()).observe(viewLifecycleOwner, resObserver)
                 }
 
                 return@setOnEditorActionListener true
@@ -59,65 +74,45 @@ class StatoTrenoFragment : Fragment() {
         val searchButton = view.findViewById<Button>(R.id.search_button)
         searchButton.setOnClickListener{
             if (textView.text.toString().isNotEmpty()) {
-                searchTrain(textView.text.toString())
+                model.searchTrain(textView.text.toString()).observe(viewLifecycleOwner, resObserver)
             }
         }
 
         return  view
     }
 
-    private fun searchTrain(trainID : String) {
-        val resObserver = Observer<String> {res ->
-            if (res.isEmpty()) {
-                val trainView = (view as View).findViewById<LinearLayout>(R.id.train_description)
-                trainView.visibility = View.INVISIBLE
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        if (savedInstanceState != null) {
+            val oldVisibility = savedInstanceState.getInt("vis")
+            val trainView = view.findViewById<LinearLayout>(R.id.train_description)
+            trainView.visibility = oldVisibility
 
-                val contextView = (view as View).findViewById<View>(R.id.coordinator_layout)
-                Snackbar.make(contextView, "ID non valido", Snackbar.LENGTH_SHORT)
-                    .setAction("Chiudi") {}
-                    .show()
-                return@Observer
+            if (oldVisibility == View.VISIBLE){
+                val info = model.getTrainState() ?: return
+                updateTrainInfo(info)
             }
-
-            parseTrainInfo(trainID, res)
         }
-        model.searchTrain(trainID).observe(viewLifecycleOwner, resObserver)
+
     }
 
-    private fun parseTrainInfo(trainID: String, trainInfo : String) {
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        val v = (view as View).findViewById<LinearLayout>(R.id.train_description)
+        outState.putInt("vis", v.visibility)
+    }
 
-        val tmp = trainInfo.replace("\n", "").split("|")
-        val trainState = tmp[0]
-        val trainRoute = tmp[1]
-
-        val trainStops = trainRoute.split(",{") as MutableList<String>
-        val it = trainStops.listIterator()
-        var currentStationIndex = 0
-        it.forEach { station ->
-            val stationName = station.substringAfter("stazione\":\"").substringBefore("\",")
-            if (station.indexOf("Corrente\":true,") != -1) {
-                currentStationIndex = it.nextIndex()-1
-            }
-
-            it.set(stationName)
-        }
-
-        val delay = trainState.substringAfter("],\"ritardo\":").substringBefore(",")
-        val lastDetectionInfo = trainState.substringAfter("oraUltimoRilevamento\":")
-        val lastDetectionTime = lastDetectionInfo.substringBefore(",")
-        val lastDetectionStation = lastDetectionInfo.substringAfter("stazioneUltimoRilevamento\":\"").substringBefore("\",")
-
+    private fun updateTrainInfo(info : HTTParser.TrainInfo) {
         val trainView = (view as View).findViewById<LinearLayout>(R.id.train_description)
-        trainView.findViewById<TextView>(R.id.train_number).text = trainID
-        trainView.findViewById<TextView>(R.id.train_position).text = lastDetectionStation
-        trainView.findViewById<TextView>(R.id.time_last_detection).text = getDate(lastDetectionTime)
-        trainView.findViewById<TextView>(R.id.train_route).text = "${trainStops.first()} - ${trainStops.last()}"
-        trainView.findViewById<TextView>(R.id.delay).text = if (delay.toInt() >= 0) "+$delay min" else "$delay min"
-        addStationsBar(trainView, trainStops, currentStationIndex)
+        trainView.findViewById<TextView>(R.id.train_number).text = info.trainID
+        trainView.findViewById<TextView>(R.id.train_position).text = info.lastDetectionStation
+        trainView.findViewById<TextView>(R.id.time_last_detection).text = getDate(info.lastDetectionTime)
+        trainView.findViewById<TextView>(R.id.train_route).text = getString(R.string.train_route, info.stops.first().stationName, info.stops.last().stationName)
+        trainView.findViewById<TextView>(R.id.delay).text = if (info.delay.toInt() >= 0) "+${info.delay} min" else "${info.delay} min"
+        addStationsBar(trainView, info.stops, info.currentIndex)
 
         crossfade(trainView)
     }
-
 
     private fun getDate(date : String) : String {
         return when (date) {
@@ -126,23 +121,30 @@ class StatoTrenoFragment : Fragment() {
         }
     }
 
-    private fun addStationsBar(trainView: View, stations : MutableList<String>, currentIndex : Int) {
-
-        val stepBar = trainView.findViewById<SeekBar>(R.id.step_bar)
-
-        stepBar.setOnTouchListener { _, _ -> true}
-
-        stepBar.progress = currentIndex
-        stepBar.max = stations.size-1
-        stepBar.minWidth = (stations.size-1)*300
-
+    private fun addStationsBar(trainView: View, stations : MutableList<HTTParser.StationInfo>, currentIndex : Int) {
         val labelsLayout = trainView.findViewById<LinearLayout>(R.id.labels_layout)
         labelsLayout.removeAllViews()
 
         for (i in 0 until stations.size) {
-            val label = TextView(trainView.context)
-            label.text = stations[i]
-            label.setPadding(0, 0, 10, 0)
+            val label = layoutInflater.inflate(R.layout.labels_layout, trainView as ViewGroup, false)
+            label.findViewById<TextView>(R.id.station_name).text = stations[i].stationName
+            label.findViewById<TextView>(R.id.scheduled_departure_value).text = getDate(stations[i].scheduledDeparture)
+            label.findViewById<TextView>(R.id.scheduled_arrival_value).text = getDate(stations[i].scheduledArrival)
+
+            if (i <= currentIndex) {
+                label.findViewById<TextView>(R.id.departure_text).text = getString(R.string.real_departure)
+                label.findViewById<TextView>(R.id.arrival_text).text = getString(R.string.real_arrival)
+                label.findViewById<TextView>(R.id.departure_value).text = getDate(stations[i].realDeparture)
+                label.findViewById<TextView>(R.id.arrival_value).text = getDate(stations[i].realArrival)
+
+            } else {
+                label.findViewById<TextView>(R.id.departure_text).text = getString(R.string.expected_departure)
+                label.findViewById<TextView>(R.id.arrival_text).text = getString(R.string.expected_arrival)
+                label.findViewById<TextView>(R.id.departure_value).text = getDate(stations[i].expectedDeparture)
+                label.findViewById<TextView>(R.id.arrival_value).text = getDate(stations[i].expectedArrival)
+                Log.e("Orari", "index: $i, partenza: ${stations[i].expectedDeparture} ed arrivo: ${stations[i].expectedArrival}")
+            }
+
             labelsLayout.addView(label)
 
             if (i == stations.size-1) {
@@ -155,7 +157,7 @@ class StatoTrenoFragment : Fragment() {
     }
 
     private fun getLayoutParams(weight: Float): LinearLayout.LayoutParams {
-        return LinearLayout.LayoutParams(300, LinearLayout.LayoutParams.WRAP_CONTENT, weight)
+        return LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT, weight)
     }
 
     private fun crossfade(contentView : View) {
@@ -184,6 +186,4 @@ class StatoTrenoFragment : Fragment() {
                 })
         }
     }
-
-
 }
